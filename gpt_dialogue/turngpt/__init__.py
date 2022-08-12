ad83a4855b2e2474dd756d28cad411da6678a604
Muhammad Umair <umair.aarij@gmail.com>
2022-08-12 12:06:28 -0400

    Defined common API for TurnGPT and MonologueGPT

---
+++ gpt_dialogue/turngpt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2022-07-27 09:51:59
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2022-08-11 16:32:07
+
+
+from gpt_dialogue.turngpt.turngpt import TurnGPT
