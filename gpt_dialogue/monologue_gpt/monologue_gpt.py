c33ba816d2506b0b30a6e1561c1736bd816ea023
Muhammad Umair <umair.aarij@gmail.com>
2022-12-04 11:51:07 +0500

    Resolved MonologueGPT Null model issue

--- gpt_dialogue/monologue_gpt/monologue_gpt.py
+++ gpt_dialogue/monologue_gpt/monologue_gpt.py
@@ -2,7 +2,7 @@
 # @Author: Muhammad Umair
 # @Date:   2022-08-11 15:55:27
 # @Last Modified by:   Muhammad Umair
-# @Last Modified time: 2022-12-03 02:33:23
+# @Last Modified time: 2022-12-04 01:48:31
 
 import os
 from typing import Union, List
@@ -24,6 +24,8 @@ from datasets import load_dataset
 from gpt_dialogue.model import LanguageModel
 from gpt_dialogue.monologue_gpt.dm import MonologueGPTFinetuneDM
 
+from typing import Dict
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -34,7 +36,8 @@ class MonologueGPT(LanguageModel):
 
     _SUPPORTED_MODELS = (
         "gpt2",
-        "gpt2-large"
+        "gpt2-large",
+        "distillgpt2"
     )
 
 
@@ -53,25 +56,64 @@ class MonologueGPT(LanguageModel):
         tokenizer_checkpoint : str = "gpt2",
         tokenizer_pad_token :str = None,
         tokenizer_eos_token : str = None,
-        tokenizer_additional_special_tokens : List[str] = None
+        tokenizer_additional_special_tokens : List[str] = None,
+        tokenizer_kwargs : Dict = {},
+        model_kwargs : Dict = {}
     ):
+
+        # Verify that the model is supported or that the path exists
+        if not os.path.exists(model_checkpoint) and \
+                not model_checkpoint in self._SUPPORTED_MODELS:
+            logger.error(
+                    "ERROR: Provide a valid path or selected pre-trained "
+                    f"models from {self._SUPPORTED_MODELS}"
+                )
+
         self.model_checkpoint = model_checkpoint
         self.tokenizer_checkpoint = tokenizer_checkpoint
 
+        # Create the tokenizer args
+        tokenizer_args = {
+            "pretrained_model_name_or_path" : tokenizer_checkpoint,
+            "pad_token" : tokenizer_pad_token,
+            "eos_token" : tokenizer_eos_token,
+            "additional_special_tokens" : tokenizer_additional_special_tokens
+        }
+        tokenizer_args.update(tokenizer_kwargs)
+        # Remove all the None args
+        tokenizer_args = {
+            k:v for k,v in tokenizer_args.items() if v != None
+        }
         # Load tokenizer
-        self._tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_checkpoint,
-            # pad_token=tokenizer_pad_token,
-            # eos_token=tokenizer_eos_token,
-            # additional_special_tokens=tokenizer_additional_special_tokens
-            )
+        self._tokenizer = AutoTokenizer.from_pretrained(**tokenizer_args)
 
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_checkpoint,
-            # pad_token_id = self._tokenizer.pad_token_id,
-            # eos_token_id = self._tokenizer.eos_token_id
-        )
-        # self.model.resize_token_embeddings(len(self._tokenizer))
+        # Create the model args
+        model_args = {
+            "pretrained_model_name_or_path" : model_checkpoint,
+        }
+        if tokenizer_pad_token != None:
+            model_args["pad_token_id"] = self._tokenizer.pad_token_id
+        if tokenizer_eos_token != None:
+            model_args["eos_token_id"] = self._tokenizer.eos_token_id
+        model_args.update(model_kwargs)
+        # Remove all the None args
+        model_args = {
+            k:v  for k,v in model_args.items() if v != None
+        }
+        # Load the model
+        self.model = AutoModelForCausalLM.from_pretrained(**model_args)
+        self.model.resize_token_embeddings(len(self._tokenizer))
+
+        # If there is only pretrained_model_name_or_path arg.
+        if len(model_args) == 1 and len(tokenizer_args) == 1 and \
+                model_args["pretrained_model_name_or_path"] in self._SUPPORTED_MODELS and \
+                tokenizer_args["pretrained_model_name_or_path"] in self._SUPPORTED_MODELS:
+            logger.warning(
+                f"WARNING: Loading pretrained model {model_checkpoint} "\
+                f"and tokenizer {tokenizer_checkpoint} with "
+                f"default arguments.\nThis is equivalent to using the specified "\
+                f"model and tokenizer directly from the transformers library."
+            )
 
     def finetune(
         self,
